ee2846ee61b060dffc2022bce1b8f6e1d44e2393
ivsr <ivsr.set.hust@gmail.com>
2022-08-05 12:04:01 +0700

    modification to run NetworkTestClient as Python module

---
+++ commands.sh
@@ -0,0 +1,9 @@
+python3 ./train.py -pb /media/data2/teamICRA -db /media/data2/teamICRA/X4Gates_Circles_rl18tracks -n X4Gates_Circle_l100
+python3 ./test_recurrent.py -pb /media/data2/teamICRA -db /media/data2/teamICRA/X4Gates_Circles_rl18tracks -n X4Gates_Circle_l100 -j8
+python3 ./test_dataloader.py -pb /media/data2/teamICRA -db /media/data2/teamICRA/X4Gates_Circles_rl18tracks -n X4Gates_Circle_l100 -j8
+python3 ./train_recurrent.py -pb /media/data2/teamICRA -db /media/data2/teamICRA/X4Gates_Circles_rl18tracks -n X4Gates_Circle_l100 -j2
+
+
+python -m orb_imitation.datagen.NetworkTestClient -arc resnet8 -w /media/data2/teamICRA/runs/ResNet8_bs=32_lt=MSE_lr=0.001_c=baseline/best.pth
+python -m orb_imitation.datagen.RaceNet8TestClient -arc racenet8 -w /media/data2/teamICRA/runs/RaceNet8-Saturday/best.pth
+python -m orb_imitation.datagen.RaceNet8TestClient -arc racenet8 -w /media/data2/teamICRA/runs/RaceNet8_all=32_lt=MSE_lr=0.001_c=run0/best.pth
