0cb706bc5c546f3931ee95fa6267e112b6f1e320
Micha Heiß <mhlauffen@web.de>
2022-08-19 09:07:57 +0200

    mean&std for X1Gate200

--- config.py
+++ config.py
@@ -2,15 +2,15 @@
 
 import torchvision.transforms as transforms
 
-device = 'cpu'
+device = 'cuda'
 epochs = 100
 learning_rate = 0.001
 learning_rate_change = 0.1
 learning_rate_change_epoch = 5
 batch_size = 32
 resnet_factor = 0.25
-num_train_tracks = 37
-num_val_tracks = 15
+num_train_tracks = 150
+num_val_tracks = 50
 jobs = 8
 
 input_channels = {
@@ -19,7 +19,7 @@ input_channels = {
     'orb': False,
 }
 
-TB_suffix = "run0"
+TB_suffix = "run1"
 loss_type = "MSE"
 phases = ['train', 'val']
 skipFirstXImages = 0  # 60
@@ -32,8 +32,11 @@ project_basepath = "/home/micha/dev/ml/orb_imitation"
 dataset_basepath = "/data/datasets"
 # dataset_basename = "X4Gates_Circle_right_"
 # dataset_basename = "X4Gates_Circles"
-dataset_basename = "X1GateDepth"
+dataset_basename = "X1Gate200"
 # dataset_basename = "X4Gates_Circle_2"
+
+dataset_mean = (0.4697,  0.4897,  0.4988, 49.4976)
+dataset_std = (2.7118e-01, 2.9868e-01, 3.3708e-01, 6.8829e+02)
 
 num_input_channels = (input_channels['rgb'] * 3) + \
                      (input_channels['depth'] * 1) + \
@@ -58,18 +61,18 @@ tf = None
 if itypes == 'rgb' or itypes == 'rgbo':
     tf = transforms.Compose([
         transforms.Normalize(
-            (0.4694, 0.4853, 0.4915),
-            (0.2693, 0.2981, 0.3379))
+            dataset_mean[:3],
+            dataset_std[:3])
     ])
 elif itypes == 'rgbd' or itypes == 'rgbdo':
     tf = transforms.Compose([
         transforms.Normalize(
-            (0.4694,  0.4853,  0.4915, 49.0911),
-            (2.6934e-01, 2.9809e-01, 3.3785e-01, 6.9964e+02))
+            dataset_mean,
+            dataset_std)
     ])
 elif itypes == 'd' or itypes == 'do':
     tf = transforms.Compose([
         transforms.Normalize(
-            (49.0911),
-            (6.9964e+02))
+            dataset_mean[3],
+            dataset_std[3])
     ])
