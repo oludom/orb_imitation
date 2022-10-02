bbaa41dece56e8d4f13a73fa21b3d967cf04dc44
ivsr <ivsr.set.hust@gmail.com>
2022-10-02 10:47:12 +0700

    Resnet16 + DAgger

--- train_dagger.py
+++ train_dagger.py
@@ -1,25 +1,31 @@
+from re import I
 import sys
 
 from sklearn.utils import shuffle
 
 sys.path.insert(0, './datagen')
 
-from DaggerClient import DaggerClient
+from datagen.DaggerClient import DaggerClient
 from train import *  # also imports config
 
 from copy import deepcopy
 import torch
-
+import math
+from GateNet import RaceGateNet
 from ResNet8 import ResNet8
 import torch.backends.cudnn as cudnn
 import torch.nn as nn
+import wandb
 
 if __name__ == "__main__":
 
     import contextlib
 
-    beta = 1
-
+    beta = 0.7
+    weight = 0.1
+    beta_rate = 0.52
+    def schedular_rate(round):
+        return (weight) /((round +1) * beta_rate)
     # tb path handling
     TB_path, writer = get_path_for_run(config.project_basepath, config.dataset_basename, config.itypes,
                                        config.resnet_factor, config.batch_size, config.loss_type, config.learning_rate,
@@ -28,6 +34,7 @@ if __name__ == "__main__":
     dev = torch.device(config.device)
 
     model = ResNet8(input_dim=config.num_input_channels, output_dim=4, f=config.resnet_factor)
+    # model = RaceGateNet()
     model = model.to(dev)
 
     if config.parallel and config.device == 'cuda':
@@ -65,14 +72,20 @@ if __name__ == "__main__":
         step_pos[el] = 0
 
     best_model = copy.deepcopy(model.state_dict())
-    best_loss = 0.0
-
+    best_loss = math.inf
+    agent_round = 0 
     try:
         # for each configuration
         for i, gateConfig in enumerate(configurations):
-
+            i_contitnue = i + 36
+            wandb.init(project="Resnet16_rgbsparse_new_planner", entity="dungtd2403")
+            # wandb.config = {
+            #         "learning_rate": 0.001,
+            #         "epochs": 2,
+            #         "batch_size": 32
+            # }
             # use i + 1 if model is not pretrained
-            beta = 1 / (i+2)
+            # beta = 1 / (i+2)
 
             with contextlib.closing(
                 DaggerClient(model, beta=beta, device=config.device, raceTrackName=f"track{i+config.skip_tracks}", configFilePath='datagen/config_dr.json', createDataset=True)) as dc:
@@ -93,9 +106,37 @@ if __name__ == "__main__":
             for el in config.phases:
                 batch_count[el] = len(datasets[el])
                 print(f"batch count {el}: {batch_count[el]}")
-                                
-            model = train_epoch(i + config.epoch_start, len(configurations) + config.epoch_start, model, config.phases, config.learning_rate, config.learning_rate_change, config.learning_rate_change_epoch, datasets, dev, lossfunction, writer, step_pos, TB_path, batch_count)
             
+            wandb.watch(model, log_freq=100)
+            best_epoch = 0
+            for e in range(config.epochs):             
+                model , loss = train_epoch(e + config.epochs * i_contitnue, len(configurations) * config.epochs, model, config.phases, config.learning_rate, config.learning_rate_change, config.learning_rate_change_epoch, datasets, dev, lossfunction, writer, step_pos, TB_path, batch_count)
+                if loss < best_loss:
+                    print("Updating best model")
+                    best_loss = loss
+                    best_epoch = e
+                    current_model = copy.deepcopy(model.state_dict())
+                    
+            torch.save(current_model, str(TB_path) + f"/round{i_contitnue}.pth")
+            
+            # beta_index to reduce beta rate
+            i_beta= i + 1
+
+            # if no pretrain => init number of expert round
+            init_round = 0 
+            wandb.log({f"beta round{i_beta + 36}": beta})
+            if i > init_round:
+                change_rate = schedular_rate(i_beta)
+                agent_round = 0 
+                beta -= change_rate
+
+            if beta <= 0:
+                agent_round += 1
+                beta = 0.0
+            if agent_round == 2:
+                break
+            # wandb.log({f"beta round{i}": beta})
+            wandb.finish()
 
     except Exception as e:
         raise e
